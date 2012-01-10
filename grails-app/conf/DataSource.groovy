f5a8da0ed8e00eae2f8a147e974690c5ac84267d
Steve Vlaminck <steve@refactr.com>
2012-01-10 08:22:11 -0500

    updated dataSource to update the database instead of create it

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -25,7 +25,7 @@ environments {
     }
     production {
         dataSource {
-            dbCreate = "create"
+            dbCreate = "update"
 			url = "jdbc:mysql://localhost/kands"
 			driverClassName = "com.mysql.jdbc.Driver"
 			username = "mealplanner"
